79d886f7e095c659f3b682f1c5e45427e9f6d7e2
Lencodes <contact@lencodes.com>
2022-10-07 12:20:00 +0200

    INIT PIP LAYOUT

--- pwa/src/skeleton-implementations/website/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { LandingTemplate } from "../templates/landing/LandingTemplate";
-
-const IndexPage: React.FC = () => {
-  return <LandingTemplate />;
-};
-
-export default IndexPage;
