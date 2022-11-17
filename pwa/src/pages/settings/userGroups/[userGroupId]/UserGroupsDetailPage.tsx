8171dc18061d85c0e42b3ced30df33c622ae1f63
Remko <remko@conduction.nl>
2022-11-17 15:13:29 +0100

    fixed build

--- pwa/src/pages/settings/userGroups/[userGroupId]/UserGroupsDetailPage.tsx
+++ pwa/src/pages/settings/userGroups/[userGroupId]/UserGroupsDetailPage.tsx
@@ -1,14 +1,7 @@
 import * as React from "react";
 import { PageProps } from "gatsby";
 import { DashboardTemplate } from "../../../../templates/dashboard/DashboardTemplate";
-import { UserGroupsDetailTemplate } from "../../../../templates/userGroupsDetailTemplate/UserGroupsDetailsTemplate";
-import { CreateUserGroupFormTemplate } from "../../../../templates/templateParts/userGroupsForm/CreateUserGroupFormTemplate";
 
-const UserGroupsDetailPage: React.FC<PageProps> = (props: PageProps) => (
-  <DashboardTemplate>
-    {props.params.userGroupId === "new" && <CreateUserGroupFormTemplate />}
-    {props.params.userGroupId !== "new" && <UserGroupsDetailTemplate userGroupId={props.params.userGroupId} />}
-  </DashboardTemplate>
-);
+const UserGroupsDetailPage: React.FC<PageProps> = (props: PageProps) => <DashboardTemplate></DashboardTemplate>;
 
 export default UserGroupsDetailPage;
