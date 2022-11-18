1bc0333d2194b5e52fecb6670f8e7f8a8393364b
Remko <remko@conduction.nl>
2022-11-18 14:04:30 +0100

    build error

--- pwa/src/pages/settings/userGroups/[userGroupId]/UserGroupsDetailPage.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import { PageProps } from "gatsby";
-import { DashboardTemplate } from "../../../../templates/dashboard/DashboardTemplate";
-
-const UserGroupsDetailPage: React.FC<PageProps> = (props: PageProps) => <DashboardTemplate></DashboardTemplate>;
-
-export default UserGroupsDetailPage;
