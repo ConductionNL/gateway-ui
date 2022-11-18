7a309aa103680737e5dbf317f8c563d95389ebc0
Thijn <Ralkey@outlook.com>
2022-11-18 13:12:25 +0100

    added GatewayDetail tab

--- pwa/src/templates/gatewayDetailTemplate/GatewayDetailTemplate.tsx
+++ pwa/src/templates/gatewayDetailTemplate/GatewayDetailTemplate.tsx
@@ -1,21 +1,16 @@
 import * as React from "react";
 import * as styles from "./GatewayDetailTemplate.module.css";
-import { Button, Heading1, Heading2, Link, Paragraph } from "@gemeente-denhaag/components-react";
+import { Button, Heading1, Heading2, Heading3, Paragraph } from "@gemeente-denhaag/components-react";
 import { useTranslation } from "react-i18next";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@gemeente-denhaag/table";
-import { navigate } from "gatsby";
 import { Container, Tag, ToolTip } from "@conduction/components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { TEMPORARY_GATEWAYDETAIL } from "../../data/gatewayDetail";
-import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { faArrowsRotate } from "@fortawesome/free-solid-svg-icons";
 import _ from "lodash";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@gemeente-denhaag/table";
 
 export const GatewayDetailTemplate: React.FC = () => {
   const { t, i18n } = useTranslation();
-
-  //   const queryClient = new QueryClient();
-  //   const _useUserGroup = useUserGroup(queryClient);
-  //   const getUserGroups = _useUserGroup.getAll();
 
   const gatewayDetails = TEMPORARY_GATEWAYDETAIL;
 
@@ -25,7 +20,7 @@ export const GatewayDetailTemplate: React.FC = () => {
         <Heading1>{t("Gateway Detail")}</Heading1>
         <div className={styles.buttons}>
           <Button className={styles.buttonIcon}>
-            <FontAwesomeIcon icon={faChevronUp} />
+            <FontAwesomeIcon icon={faArrowsRotate} />
             {t("Upgrade")}
           </Button>
         </div>
@@ -34,48 +29,103 @@ export const GatewayDetailTemplate: React.FC = () => {
       {/* {getUserGroups.isError && "Error..."} */}
 
       {/* {getUserGroups.isSuccess && ( */}
-      {/* <Table>
-        <TableHead>
-          <TableRow>
-            <TableHeader>{t("Name")}</TableHeader>
-            <TableHeader>{t("Description")}</TableHeader>
-            <TableHeader>{t("Versions")}</TableHeader>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {gatewayDetails.map((gatewayDetail) => (
-            <TableRow className={styles.tableRow} key={gatewayDetail.name}>
-              <TableCell>{gatewayDetail.name}</TableCell>
-              <TableCell>{gatewayDetail.description ?? "-"}</TableCell>
-              <TableCell>{gatewayDetail.versions.join(", ") ?? "-"}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table> */}
-
       {gatewayDetails.map((gatewayDetail) => (
-        <>
-          <Heading2>{gatewayDetail.name}</Heading2>
+        <div className={styles.gatewayDetailContainer}>
+          <div>
+            <Heading2>{gatewayDetail.name}</Heading2>
 
-          <div className={styles.type}>
-            <p>{`Type: ${gatewayDetail.type}`}</p>
-            <p>{`Names: ${gatewayDetail.names.join(", ")}`}</p>
-          </div>
+            <div className={styles.type}>
+              <p>{`Type: ${gatewayDetail.type}`}</p>
+              <p>{`Names: ${gatewayDetail.names.join(", ")}`}</p>
+              <p>{`Versions: ${gatewayDetail.versions.join(", ")}`}</p>
+            </div>
 
-          <div className={styles.descriptionAndTags}>
-            <Paragraph>{gatewayDetail.description}</Paragraph>
+            <div className={styles.descriptionAndTags}>
+              <Paragraph className={styles.description}>{gatewayDetail.description}</Paragraph>
 
-            <div className={styles.tags}>
-              {gatewayDetail?.licenses &&
-                gatewayDetail.licenses.length > 0 &&
-                gatewayDetail.licenses.map((license) => (
-                  <ToolTip tooltip={`License ${license?.osi}`}>
-                    <Tag label={_.upperFirst(license?.name)} onClick={() => open(license?.url)} />
+              <div className={styles.tags}>
+                {gatewayDetail?.licenses &&
+                  gatewayDetail.licenses.length > 0 &&
+                  gatewayDetail.licenses.map((license) => (
+                    <ToolTip tooltip={`License ${license?.osi}`}>
+                      <Tag label={_.upperFirst(license?.name)} onClick={() => open(license?.url)} />
+                    </ToolTip>
+                  ))}
+
+                {gatewayDetail?.source && (
+                  <ToolTip tooltip={`Source ${gatewayDetail.source?.reference}`}>
+                    <Tag
+                      label={_.upperFirst(gatewayDetail.source?.type)}
+                      onClick={() => open(gatewayDetail.source?.url)}
+                    />
                   </ToolTip>
-                ))}
+                )}
+
+                {gatewayDetail?.dist && (
+                  <ToolTip tooltip={`Dist ${gatewayDetail.dist?.reference}`}>
+                    <Tag label={_.upperFirst(gatewayDetail.dist?.type)} onClick={() => open(gatewayDetail.dist?.url)} />
+                  </ToolTip>
+                )}
+              </div>
             </div>
           </div>
-        </>
+
+          <div className={styles.extraInfo}>
+            <div className={styles.extraInfoTable}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    {gatewayDetail?.requires && <TableHeader>requires</TableHeader>}
+                    {gatewayDetail?.devRequires && <TableHeader>dev requires</TableHeader>}
+                    {gatewayDetail?.autoload && <TableHeader>autoload</TableHeader>}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow>
+                    {gatewayDetail?.requires && (
+                      <TableCell>
+                        {Object.entries(gatewayDetail.requires).map(([key, value]) => (
+                          <>
+                            {key} {value} <br />
+                          </>
+                        ))}
+                      </TableCell>
+                    )}
+                    {gatewayDetail?.devRequires && (
+                      <TableCell>
+                        {Object.entries(gatewayDetail.devRequires).map(([key, value]) => (
+                          <>
+                            {key} {value} <br />
+                          </>
+                        ))}
+                      </TableCell>
+                    )}
+                    {gatewayDetail?.autoload && (
+                      <TableCell>
+                        {Object.entries(gatewayDetail.autoload).map(([key, value]) => (
+                          <>
+                            {key} {Object.keys(value)} <br />
+                          </>
+                        ))}
+                      </TableCell>
+                    )}
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </div>
+            <div className={styles.extraInfoSupport}>
+              <Heading3>Support</Heading3>
+              <div>
+                {gatewayDetail.support.source && (
+                  <Button onClick={() => open(gatewayDetail.support.source)}>Source</Button>
+                )}
+                {gatewayDetail.support.issues && (
+                  <Button onClick={() => open(gatewayDetail.support.issues)}>Issues</Button>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
       ))}
       {/* )} */}
 
