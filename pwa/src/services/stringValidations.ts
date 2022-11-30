98755cef1374a08c73c2cb9849f8f95afe66ff46
Remko <remko@conduction.nl>
2022-11-30 15:49:24 +0100

    fixed broken crontab validator

--- pwa/src/services/stringValidations.ts
+++ pwa/src/services/stringValidations.ts
@@ -1,11 +1,10 @@
+import { isValidCron } from "cron-validator";
+
 export const validateStringAsCronTab = (crontab: string): boolean | string => {
   if (!crontab) return true;
 
-  const cronregex = new RegExp(
-    /^(\*|([0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9])|\*\/([0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9])) (\*|([0-9]|1[0-9]|2[0-3])|\*\/([0-9]|1[0-9]|2[0-3])) (\*|([1-9]|1[0-9]|2[0-9]|3[0-1])|\*\/([1-9]|1[0-9]|2[0-9]|3[0-1])) (\*|([1-9]|1[0-2])|\*\/([1-9]|1[0-2])) (\*|([0-6])|\*\/([0-6]))$/,
-  );
-
-  if (cronregex.test(crontab)) {
+  if (isValidCron(crontab)) {
+    console.log(isValidCron(crontab));
     return true;
   } else {
     return "This is not valid crontab. see https://crontab.guru/ to create an interval.";
