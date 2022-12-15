1f71b72c4f3f54a33a9d81f564f3157c12903c04
Thijn <Ralkey@outlook.com>
2022-12-15 13:16:15 +0100

    WIP. added downloads field to gateway detail

--- pwa/src/templates/gatewayDetailTemplate/GatewayDetailTemplate.tsx
+++ pwa/src/templates/gatewayDetailTemplate/GatewayDetailTemplate.tsx
@@ -1,15 +1,17 @@
 import * as React from "react";
 import * as styles from "./GatewayDetailTemplate.module.css";
-import { Button, Heading1, Heading2, Heading3, Paragraph } from "@gemeente-denhaag/components-react";
+import { Button, Heading1, Heading2, Heading3, Heading4, Link, Paragraph } from "@gemeente-denhaag/components-react";
 import { useTranslation } from "react-i18next";
 import { Container } from "@conduction/components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowsRotate, faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { faArrowsRotate } from "@fortawesome/free-solid-svg-icons";
 import _ from "lodash";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@gemeente-denhaag/table";
 import { QueryClient } from "react-query";
 import { usePlugin } from "../../hooks/plugin";
 import Skeleton from "react-loading-skeleton";
+import { CircleIndicatorTemplate } from "../templateParts/ratingIndicator/CircleIndicatorTemplate";
+import { ExternalLinkIcon } from "@gemeente-denhaag/icons";
 
 export const GatewayDetailTemplate: React.FC = () => {
   const { t } = useTranslation();
@@ -33,35 +35,59 @@ export const GatewayDetailTemplate: React.FC = () => {
       {getPlugins.isError && "Error..."}
 
       {getPlugins.isSuccess && (
-        <>
-          <div className={styles.gatewayDetailContainer}>
-            <div>
-              <Heading2>{getPlugins.data?.name}</Heading2>
+        <div className={styles.gatewayDetailContainer}>
+          <div>
+            <Heading2>{getPlugins.data?.name}</Heading2>
 
-              <div className={styles.type}>
-                <p>{`Type: ${getPlugins.data?.type}`}</p>
-                <p>{`Language: ${getPlugins.data?.language}`}</p>
-              </div>
+            <div className={styles.type}>
+              <p>{`Type: ${getPlugins.data?.type}`}</p>
+              <p>{`Language: ${getPlugins.data?.language}`}</p>
+              <p>{`Dependents: ${getPlugins.data?.dependents}`}</p>
+            </div>
 
-              <div className={styles.descriptionAndRepo}>
-                <Paragraph>{getPlugins.data?.description}</Paragraph>
+            <div className={styles.descriptionAndRepo}>
+              <Paragraph>{getPlugins.data?.description}</Paragraph>
 
-                <div>
-                  {getPlugins.data.repository && (
-                    <Button
-                      icon={<FontAwesomeIcon icon={faArrowUpRightFromSquare} />}
-                      onClick={() => open(getPlugins.data.repository)}
-                    >
-                      Repository
-                    </Button>
-                  )}
-                </div>
-              </div>
-
-              <></>
+              <Paragraph>
+                go to{" "}
+                <span onClick={() => open(getPlugins.data.repository)}>
+                  <Link icon={<ExternalLinkIcon />}>
+                    repository
+                  </Link>
+                </span>
+              </Paragraph>
             </div>
           </div>
-        </>
+
+          <Heading3>Downloads</Heading3>
+          <div className={styles.downloads}>
+            <div>
+              <Heading4>Total</Heading4>
+              <CircleIndicatorTemplate
+                layoutClassName={styles.downloadIndicatorContainer}
+                value={getPlugins.data?.downloads.total}
+              />
+            </div>
+
+            <div>
+              <Heading4>Monthly</Heading4>
+              <CircleIndicatorTemplate
+                layoutClassName={styles.downloadIndicatorContainer}
+                maxValue={getPlugins.data?.downloads.total}
+                value={getPlugins.data?.downloads.monthly}
+              />
+            </div>
+
+            <div>
+              <Heading4>Daily</Heading4>
+              <CircleIndicatorTemplate
+                layoutClassName={styles.downloadIndicatorContainer}
+                maxValue={getPlugins.data?.downloads.total}
+                value={getPlugins.data?.downloads.daily}
+              />
+            </div>
+          </div>
+        </div>
       )}
 
       {getPlugins.isLoading && <Skeleton height="200px" />}
