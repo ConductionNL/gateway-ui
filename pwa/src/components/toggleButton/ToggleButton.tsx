5f6ade9fb02139a4d716cb45ad1c2b6acbfdce8f
Thijn <Ralkey@outlook.com>
2023-01-05 12:09:11 +0100

    WIP finished toggle button for now

--- pwa/src/components/toggleButton/ToggleButton.tsx
+++ pwa/src/components/toggleButton/ToggleButton.tsx
@@ -3,18 +3,47 @@ import * as styles from "./ToggleButton.module.css";
 import clsx from "clsx";
 
 interface ToggleButtonProps {
+  startLabel?: string;
+  endLabel?: string;
+  checked?: boolean;
+  onChange?: () => any;
+  onEnabled?: () => any;
+  onDisabled?: () => any;
   layoutClassName?: any;
 }
 
-const ToggleButton: React.FC<ToggleButtonProps> = ({ layoutClassName }) => {
-  const uuid = Math.floor(Math.random() * Date.now()).toString()
+const ToggleButton: React.FC<ToggleButtonProps> = ({
+  startLabel,
+  endLabel,
+  checked,
+  onChange,
+  onEnabled,
+  onDisabled,
+  layoutClassName,
+}) => {
+  const [isEnabled, setIsEnabled] = React.useState<boolean>(checked ?? false);
+
+  const uuid = Math.floor(Math.random() * Date.now()).toString();
+
+  const handleChange = () => {
+    onChange && onChange();
+
+    setIsEnabled(!isEnabled);
+  };
+
+  React.useEffect(() => {
+    if (!!onEnabled && isEnabled) return onEnabled();
+    if (!!onDisabled && !isEnabled) return onDisabled();
+  }, [isEnabled]);
 
   return (
-    <div className={clsx([layoutClassName && layoutClassName])}>
+    <div className={clsx(styles.container, [layoutClassName && layoutClassName])}>
+      {startLabel}
       <div className={styles.switchContainer}>
-        <input id={uuid} type="checkbox" />
+        <input id={uuid} type="checkbox" checked={isEnabled} onChange={handleChange} />
         <label htmlFor={uuid}></label>
       </div>
+      {endLabel}
     </div>
   );
 };
