6df17acd1b96333ef2d3a63bb131e64270897e26
Thijn <Ralkey@outlook.com>
2023-01-13 12:37:49 +0100

    WIP. pretty much finished

--- pwa/src/templates/templateParts/organizationsForm/EditOrganizationFormTemplate.tsx
+++ pwa/src/templates/templateParts/organizationsForm/EditOrganizationFormTemplate.tsx
@@ -1,10 +1,5 @@
 import * as React from "react";
 import * as styles from "./OrganizationFormTemplate.module.css";
-import { useForm } from "react-hook-form";
-import { Button, Heading1 } from "@gemeente-denhaag/components-react";
-import { useTranslation } from "react-i18next";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFloppyDisk } from "@fortawesome/free-solid-svg-icons";
 import { OrganizationForm } from "./OrganizationForm";
 import { QueryClient } from "react-query";
 import { useOrganization } from "../../../hooks/organization";
@@ -15,16 +10,13 @@ interface CreateOrganizationFormTemplateProps {
 }
 
 export const EditOrganizationFormTemplate: React.FC<CreateOrganizationFormTemplateProps> = ({ organizationId }) => {
-  const { t } = useTranslation();
-  const [loading, setLoading] = React.useState<boolean>(false);
-
   const queryClient = new QueryClient();
   const _useOrganizations = useOrganization(queryClient);
   const getOrganization = _useOrganizations.getOne(organizationId);
 
   return (
     <div className={styles.container}>
-      {getOrganization.isSuccess && <OrganizationForm organization={getOrganization.data} {...{ loading }} />}
+      {getOrganization.isSuccess && <OrganizationForm organization={getOrganization.data} />}
 
       {getOrganization.isLoading && <Skeleton height={200} />}
     </div>
--- pwa/src/templates/templateParts/organizationsForm/OrganizationForm.tsx
+++ pwa/src/templates/templateParts/organizationsForm/OrganizationForm.tsx
@@ -1,37 +1,46 @@
 import * as React from "react";
-import * as styles from "./SecurityGroupFormTemplate.module.css";
+import * as styles from "./OrganizationFormTemplate.module.css";
 import FormField, { FormFieldInput, FormFieldLabel } from "@gemeente-denhaag/form-field";
 import { Alert, Button, Heading1 } from "@gemeente-denhaag/components-react";
 import { useTranslation } from "react-i18next";
 import { InputText, Textarea } from "@conduction/components";
 import { useForm } from "react-hook-form";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFloppyDisk } from "@fortawesome/free-solid-svg-icons";
+import { faFloppyDisk, faMinus, faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { QueryClient } from "react-query";
 import { useOrganization } from "../../../hooks/organization";
+import clsx from "clsx";
+import { useDashboardCard } from "../../../hooks/useDashboardCard";
 
 interface OrganizationFormProps {
-  loading: any;
   organization?: any;
 }
 
-export const OrganizationForm: React.FC<OrganizationFormProps> = ({ loading, organization }) => {
+export const OrganizationForm: React.FC<OrganizationFormProps> = ({ organization }) => {
   const { t } = useTranslation();
+  const { addOrRemoveDashboardCard, getDashboardCard } = useDashboardCard();
   const [formError, setFormError] = React.useState<string>("");
+  const [loading, setLoading] = React.useState<boolean>(false);
 
   const queryClient = new QueryClient();
   const _useOrganizations = useOrganization(queryClient);
-  const createOrEditOrganization = _useOrganizations.createOrEdit(organization.id);
+  const createOrEditOrganization = _useOrganizations.createOrEdit(organization?.id);
+
+  const dashboardCard = getDashboardCard(organization?.id);
 
   const {
     register,
     handleSubmit,
-	setValue,
+    setValue,
     formState: { errors },
   } = useForm();
 
+  const addOrRemoveFromDashboard = () => {
+    // addOrRemoveDashboardCard(organization.name, "Organization", "Organization", organization.id, dashboardCard?.id);
+  };
+
   const handleSetFormValues = (organization: any): void => {
-    const basicFields: string[] = ["firstName", "lastName"];
+    const basicFields: string[] = ["name", "description"];
     basicFields.forEach((field) => setValue(field, organization[field]));
   };
 
@@ -40,22 +49,38 @@ export const OrganizationForm: React.FC<OrganizationFormProps> = ({ loading, org
   }, [organization]);
 
   const onSubmit = (data: any): void => {
-    createOrEditOrganization.mutate({ payload: data, id: organization.id });
+    createOrEditOrganization.mutate({ payload: data, id: organization?.id });
 
-    organization.id && queryClient.setQueryData(["organizations", organization.id], data);
+    organization?.id && queryClient.setQueryData(["organizations", organization.id], data);
   };
 
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
         <section className={styles.section}>
-          <Heading1>{t("Create Organization")}</Heading1>
+          <Heading1>
+            {organization?.id ? `Edit ${organization.name}` : "Create Organization"}
+          </Heading1>
 
           <div className={styles.buttons}>
             <Button className={styles.buttonIcon} type="submit" disabled={loading}>
               <FontAwesomeIcon icon={faFloppyDisk} />
               {t("Save")}
             </Button>
+
+            {organization?.id && (
+              <>
+                <Button className={styles.buttonIcon} onClick={addOrRemoveFromDashboard}>
+                  <FontAwesomeIcon icon={dashboardCard ? faMinus : faPlus} />
+                  {dashboardCard ? t("Remove from dashboard") : t("Add to dashboard")}
+                </Button>
+
+                {/* <Button className={clsx(styles.buttonIcon, styles.deleteButton)} onClick={handleDelete}>
+                  <FontAwesomeIcon icon={faTrash} />
+                  {t("Delete")}
+                </Button> */}
+              </>
+            )}
           </div>
         </section>
 
@@ -66,13 +91,6 @@ export const OrganizationForm: React.FC<OrganizationFormProps> = ({ loading, org
               <FormFieldInput>
                 <FormFieldLabel>{t("Name")}</FormFieldLabel>
                 <InputText {...{ register, errors }} name="name" validation={{ required: true }} disabled={loading} />
-              </FormFieldInput>
-            </FormField>
-
-            <FormField>
-              <FormFieldInput>
-                <FormFieldLabel>{t("Config")}</FormFieldLabel>
-                <InputText {...{ register, errors }} name="config" validation={{ required: true }} disabled={loading} />
               </FormFieldInput>
             </FormField>
 
