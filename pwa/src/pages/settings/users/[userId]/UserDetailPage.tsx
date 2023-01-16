174de1e136f76ebb1730cab1cf3b61566d6ce7c4
Thijn <Thijn@conduction.nl>
2023-01-16 14:18:18 +0100

    removed unnececary file

--- pwa/src/pages/settings/users/[userId]/UserDetailPage.tsx
+++ pwa/src/pages/settings/users/[userId]/UserDetailPage.tsx
@@ -1,14 +1,14 @@
 import * as React from "react";
 import { PageProps } from "gatsby";
 import { DashboardTemplate } from "../../../../templates/dashboard/DashboardTemplate";
-import { UserDetailsTemplate } from "../../../../templates/userDetailTemplate/UserDetailTemplate";
 import { CreateUserFormTemplate } from "../../../../templates/templateParts/userForm/CreateUserFormTemplate";
+import { EditUserFormTemplate } from "../../../../templates/templateParts/userForm/EditUserFormTemplate";
 
 const UserDetailPage: React.FC<PageProps> = (props: PageProps) => (
   <DashboardTemplate>
     {props.params.userId === "new" && <CreateUserFormTemplate />}
     {props.params.userId !== "new" && (
-      <UserDetailsTemplate userId={props.params.userId} />
+      <EditUserFormTemplate userId={props.params.userId} />
     )}
   </DashboardTemplate>
 );
