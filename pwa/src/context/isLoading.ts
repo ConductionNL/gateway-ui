607cb9ef02c0997a36fc96bfebc6cf9f27ee41cb
Lennart Bank <contact@lencodes.com>
2023-01-31 14:02:03 +0100

    REFACTOR user form states

--- pwa/src/context/isLoading.ts
+++ pwa/src/context/isLoading.ts
@@ -3,6 +3,7 @@ import * as React from "react";
 export interface IsLoadingProps {
   alert?: boolean;
   schemaForm?: boolean;
+  userForm?: boolean;
 }
 
 export const isLoading = {} as IsLoadingProps;
--- pwa/src/templates/templateParts/userForm/UserFormTemplate.tsx
+++ pwa/src/templates/templateParts/userForm/UserFormTemplate.tsx
@@ -4,35 +4,35 @@ import FormField, { FormFieldInput, FormFieldLabel } from "@gemeente-denhaag/for
 import { useTranslation } from "react-i18next";
 import { InputPassword, InputText, SelectSingle, Textarea } from "@conduction/components";
 import { useForm } from "react-hook-form";
-import { QueryClient, UseQueryResult } from "react-query";
+import { QueryClient } from "react-query";
 import { useUser } from "../../../hooks/user";
 import Skeleton from "react-loading-skeleton";
 import { validatePassword } from "../../../services/stringValidations";
 import { ErrorMessage } from "../../../components/errorMessage/ErrorMessage";
+import { useOrganization } from "../../../hooks/organization";
+import { IsLoadingContext } from "../../../context/isLoading";
 
 interface UserFormTemplateProps {
   user?: any;
-  getOrganization: UseQueryResult<any[], Error>;
 }
 
-export const UserFormTemplate: React.FC<UserFormTemplateProps> = ({ user, getOrganization }) => {
+export const formId: string = "user-form";
+
+export const UserFormTemplate: React.FC<UserFormTemplateProps> = ({ user }) => {
   const { t } = useTranslation();
+  const [isLoading, setIsLoading] = React.useContext(IsLoadingContext);
 
   const queryClient = new QueryClient();
   const _useUsers = useUser(queryClient);
   const createOrEditUser = _useUsers.createOrEdit(user?.id);
 
+  const _useOrganizations = useOrganization(queryClient);
+  const getOrganization = _useOrganizations.getAll();
+
   const organisationOptions = getOrganization.data?.map((_organisation: any) => ({
     label: _organisation.name,
     value: _organisation.id,
   }));
-
-  React.useEffect(() => {
-    setValue(
-      "organization",
-      organisationOptions?.find((_organisation) => _organisation.value === user?.organisation.id),
-    );
-  }, [getOrganization.isSuccess]);
 
   const {
     register,
@@ -51,10 +51,6 @@ export const UserFormTemplate: React.FC<UserFormTemplateProps> = ({ user, getOrg
     basicFields.forEach((field) => setValue(field, user[field]));
   };
 
-  React.useEffect(() => {
-    user && handleSetFormValues(user);
-  }, [user]);
-
   const onSubmit = (data: any): void => {
     const payload = {
       ...data,
@@ -68,42 +64,62 @@ export const UserFormTemplate: React.FC<UserFormTemplateProps> = ({ user, getOrg
     user?.id && queryClient.setQueryData(["users", user.id], data);
   };
 
+  React.useEffect(() => {
+    setIsLoading({ ...isLoading, userForm: createOrEditUser.isLoading });
+  }, [createOrEditUser.isLoading]);
+
+  React.useEffect(() => {
+    user && handleSetFormValues(user);
+  }, [user]);
+
+  React.useEffect(() => {
+    setValue(
+      "organization",
+      organisationOptions?.find((_organisation) => _organisation.value === user?.organisation.id),
+    );
+  }, [getOrganization.isSuccess]);
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)} id="UserForm" className={styles.formContainer}>
+    <form onSubmit={handleSubmit(onSubmit)} id={formId} className={styles.formContainer}>
       <div className={styles.gridContainer}>
         <div className={styles.grid}>
           <FormField>
             <FormFieldInput>
               <FormFieldLabel>{t("Name")}</FormFieldLabel>
-              <InputText {...{ register, errors }} name="name" validation={{ required: true }} />
+              <InputText
+                {...{ register, errors }}
+                name="name"
+                validation={{ required: true }}
+                disabled={isLoading.userForm}
+              />
             </FormFieldInput>
           </FormField>
 
           <FormField>
             <FormFieldInput>
               <FormFieldLabel>{t("Description")}</FormFieldLabel>
-              <Textarea {...{ register, errors }} name="description" />
+              <Textarea {...{ register, errors }} name="description" disabled={isLoading.userForm} />
             </FormFieldInput>
           </FormField>
 
           <FormField>
             <FormFieldInput>
               <FormFieldLabel>{t("Email")}</FormFieldLabel>
-              <InputText {...{ register, errors }} name="email" />
+              <InputText {...{ register, errors }} name="email" disabled={isLoading.userForm} />
             </FormFieldInput>
           </FormField>
 
           <FormField>
             <FormFieldInput>
               <FormFieldLabel>{t("Locale")}</FormFieldLabel>
-              <InputText {...{ register, errors }} name="locale" />
+              <InputText {...{ register, errors }} name="locale" disabled={isLoading.userForm} />
             </FormFieldInput>
           </FormField>
 
           <FormField>
             <FormFieldInput>
               <FormFieldLabel>{t("Person")}</FormFieldLabel>
-              <InputText {...{ register, errors }} name="person" />
+              <InputText {...{ register, errors }} name="person" disabled={isLoading.userForm} />
             </FormFieldInput>
           </FormField>
 
@@ -116,6 +132,7 @@ export const UserFormTemplate: React.FC<UserFormTemplateProps> = ({ user, getOrg
                   {...{ register, errors, control }}
                   name="organization"
                   validation={{ required: true }}
+                  disabled={isLoading.userForm}
                 />
               )}
               {getOrganization.isLoading && <Skeleton height={50} />}
@@ -129,6 +146,7 @@ export const UserFormTemplate: React.FC<UserFormTemplateProps> = ({ user, getOrg
                 {...{ register, errors }}
                 name="password"
                 validation={{ validate: () => validatePassword(pwd, pwd_verify) }}
+                disabled={isLoading.userForm}
               />
 
               {errors["password"] && <ErrorMessage message={errors["password"].message} />}
@@ -142,6 +160,7 @@ export const UserFormTemplate: React.FC<UserFormTemplateProps> = ({ user, getOrg
                 {...{ register, errors }}
                 name="password_verify"
                 validation={{ validate: () => validatePassword(pwd_verify, pwd) }}
+                disabled={isLoading.userForm}
               />
 
               {errors["password_verify"] && <ErrorMessage message={errors["password_verify"].message} />}
