3704b395675f0d48c751c121578be4416f143a06
Lennart Bank <contact@lencodes.com>
2023-02-07 13:05:54 +0100

    IMPLEMENT FormHeader in organizations

--- pwa/src/templates/templateParts/organizationsForm/CreateOrganizationTemplate.tsx
+++ pwa/src/templates/templateParts/organizationsForm/CreateOrganizationTemplate.tsx
@@ -1,10 +1,18 @@
 import * as React from "react";
 import * as styles from "./OrganizationFormTemplate.module.css";
-import { OrganizationForm } from "./OrganizationForm";
+import { OrganizationForm, formId } from "./OrganizationForm";
+import { useTranslation } from "react-i18next";
+import { IsLoadingContext } from "../../../context/isLoading";
+import { FormHeaderTemplate } from "../formHeader/FormHeaderTemplate";
 
 export const CreateOrganizationTemplate: React.FC = () => {
+  const { t } = useTranslation();
+  const [isLoading] = React.useContext(IsLoadingContext);
+
   return (
     <div className={styles.container}>
+      <FormHeaderTemplate title={t("Create Organization")} {...{ formId }} disabled={isLoading.organizationForm} />
+
       <OrganizationForm />
     </div>
   );
