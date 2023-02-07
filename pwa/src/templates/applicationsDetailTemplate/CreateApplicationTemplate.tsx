52815b9c6485a7005eb71223370fd95d9611b27f
Lennart Bank <contact@lencodes.com>
2023-02-07 12:24:19 +0100

    IMPLEMENT FormHeader in applications

--- pwa/src/templates/applicationsDetailTemplate/CreateApplicationTemplate.tsx
+++ pwa/src/templates/applicationsDetailTemplate/CreateApplicationTemplate.tsx
@@ -1,12 +1,9 @@
 import * as React from "react";
 import * as styles from "./ApplicationTemplate.module.css";
-import { Button, Heading1 } from "@gemeente-denhaag/components-react";
 import { ApplicationsFormTemplate, formId } from "../templateParts/applicationsForm/ApplicationsFormTemplate";
 import { useTranslation } from "react-i18next";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFloppyDisk } from "@fortawesome/free-solid-svg-icons";
-import clsx from "clsx";
 import { IsLoadingContext } from "../../context/isLoading";
+import { FormHeaderTemplate } from "../templateParts/formHeader/FormHeaderTemplate";
 
 export const CreateApplicationTemplate: React.FC = () => {
   const { t } = useTranslation();
@@ -14,20 +11,7 @@ export const CreateApplicationTemplate: React.FC = () => {
 
   return (
     <div className={styles.container}>
-      <section className={styles.section}>
-        <Heading1>{"Create Application"}</Heading1>
-        <div className={styles.buttons}>
-          <Button
-            form={formId}
-            type="submit"
-            disabled={isLoading.applicationForm}
-            className={clsx(styles.buttonIcon, styles.button)}
-          >
-            <FontAwesomeIcon icon={faFloppyDisk} />
-            {t("Save")}
-          </Button>
-        </div>
-      </section>
+      <FormHeaderTemplate title={t("Create Application")} {...{ formId }} disabled={isLoading.applicationForm} />
 
       <ApplicationsFormTemplate />
     </div>
