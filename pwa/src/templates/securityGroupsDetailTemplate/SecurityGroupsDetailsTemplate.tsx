45aa66eef94dce15b49d9d01d378d3937d3035f2
Lennart Bank <contact@lencodes.com>
2023-02-07 12:22:09 +0100

    IMPLEMENT FormHeader in security groups

--- pwa/src/templates/securityGroupsDetailTemplate/SecurityGroupsDetailsTemplate.tsx
+++ pwa/src/templates/securityGroupsDetailTemplate/SecurityGroupsDetailsTemplate.tsx
@@ -4,13 +4,9 @@ import { Container } from "@conduction/components";
 import { useSecurityGroup } from "../../hooks/securityGroup";
 import { QueryClient } from "react-query";
 import Skeleton from "react-loading-skeleton";
-import { faFloppyDisk, faTrash } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Heading1, Button } from "@gemeente-denhaag/components-react";
-import clsx from "clsx";
-import { t } from "i18next";
 import { IsLoadingContext } from "../../context/isLoading";
 import { SecurityGroupFormTemplate, formId } from "../templateParts/securityGroupsForm/SecurityGroupFormTemplate";
+import { FormHeaderTemplate } from "../templateParts/formHeader/FormHeaderTemplate";
 
 interface SecurityGroupsDetailPageProps {
   securityGroupId: string;
@@ -25,7 +21,7 @@ export const SecurityGroupsDetailsTemplate: React.FC<SecurityGroupsDetailPagePro
   const getSecurityGroup = _useSecurityGroups.getOne(securityGroupId);
   const deleteSecurityGroup = _useSecurityGroups.remove();
 
-  const handleDelete = (): void => {
+  const handleDeleteSecurityGroup = (): void => {
     const confirmDeletion = confirm("Are you sure you want to delete this security group?");
 
     confirmDeletion && deleteSecurityGroup.mutate({ id: securityGroupId });
@@ -42,30 +38,12 @@ export const SecurityGroupsDetailsTemplate: React.FC<SecurityGroupsDetailPagePro
 
       {getSecurityGroup.isSuccess && (
         <>
-          <section className={styles.section}>
-            <Heading1>{`Edit ${getSecurityGroup.data.name}`}</Heading1>
-
-            <div className={styles.buttons}>
-              <Button
-                type="submit"
-                form={formId}
-                disabled={isLoading.securityGroupForm}
-                className={clsx(styles.buttonIcon, styles.button)}
-              >
-                <FontAwesomeIcon icon={faFloppyDisk} />
-                {t("Save")}
-              </Button>
-
-              <Button
-                className={clsx(styles.buttonIcon, styles.button, styles.deleteButton)}
-                onClick={handleDelete}
-                disabled={isLoading.securityGroupForm}
-              >
-                <FontAwesomeIcon icon={faTrash} />
-                {t("Delete")}
-              </Button>
-            </div>
-          </section>
+          <FormHeaderTemplate
+            title={`Edit ${getSecurityGroup.data.name}`}
+            {...{ formId }}
+            disabled={isLoading.securityGroupForm}
+            handleDelete={handleDeleteSecurityGroup}
+          />
 
           <SecurityGroupFormTemplate securityGroup={getSecurityGroup.data} />
         </>
--- pwa/src/templates/templateParts/securityGroupsForm/CreateSecurityGroupFormTemplate.tsx
+++ pwa/src/templates/templateParts/securityGroupsForm/CreateSecurityGroupFormTemplate.tsx
@@ -1,12 +1,9 @@
 import * as React from "react";
 import * as styles from "./CreateSecurityGroupTemplate.module.css";
-import clsx from "clsx";
-import { Button, Heading1 } from "@gemeente-denhaag/components-react";
 import { useTranslation } from "react-i18next";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFloppyDisk } from "@fortawesome/free-solid-svg-icons";
 import { SecurityGroupFormTemplate, formId } from "./SecurityGroupFormTemplate";
 import { IsLoadingContext } from "../../../context/isLoading";
+import { FormHeaderTemplate } from "../formHeader/FormHeaderTemplate";
 
 export const CreateSecurityGroupTemplate: React.FC = () => {
   const { t } = useTranslation();
@@ -14,21 +11,7 @@ export const CreateSecurityGroupTemplate: React.FC = () => {
 
   return (
     <div className={styles.container}>
-      <section className={styles.section}>
-        <Heading1>{t("Create Security Group")}</Heading1>
-
-        <div className={styles.buttons}>
-          <Button
-            type="submit"
-            form={formId}
-            disabled={isLoading.securityGroupForm}
-            className={clsx(styles.buttonIcon, styles.button)}
-          >
-            <FontAwesomeIcon icon={faFloppyDisk} />
-            {t("Save")}
-          </Button>
-        </div>
-      </section>
+      <FormHeaderTemplate title={t("Create Security Group")} {...{ formId }} disabled={isLoading.securityGroupForm} />
 
       <SecurityGroupFormTemplate />
     </div>
