8b5675b858cd3e14bab51aa89f10f83d31e2f02d
Lennart Bank <contact@lencodes.com>
2023-02-16 15:18:30 +0100

    ADD size property to header component

--- pwa/src/templates/templateParts/overviewPageHeader/OverviewPageHeaderTemplate.tsx
+++ pwa/src/templates/templateParts/overviewPageHeader/OverviewPageHeaderTemplate.tsx
@@ -1,16 +1,23 @@
-import { Heading1 } from "@gemeente-denhaag/components-react";
+import { Heading1, Heading2 } from "@gemeente-denhaag/components-react";
 import * as React from "react";
 import * as styles from "./OverviewPageHeaderTemplate.module.css";
 
 interface OverviewPageHeaderTemplateProps {
   title: string;
   button: JSX.Element;
+
+  size?: "lg" | "md"; // defaults to "lg"
 }
 
-export const OverviewPageHeaderTemplate: React.FC<OverviewPageHeaderTemplateProps> = ({ title, button }) => {
+export const OverviewPageHeaderTemplate: React.FC<OverviewPageHeaderTemplateProps> = ({
+  title,
+  button,
+  size = "lg",
+}) => {
   return (
     <div className={styles.container}>
-      <Heading1>{title}</Heading1>
+      {size === "lg" && <Heading1>{title}</Heading1>}
+      {size === "md" && <Heading2>{title}</Heading2>}
 
       {button}
     </div>
