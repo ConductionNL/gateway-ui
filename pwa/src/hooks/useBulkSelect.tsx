98a7c02c84e7cd9ec7d8f2ff5f00a161d967979d
Lennart Bank <contact@lencodes.com>
2023-02-28 14:23:54 +0100

    ADD useBulkSelect custom hook

---
+++ pwa/src/hooks/useBulkSelect.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+
+export const useBulkSelect = (currentPage: number) => {
+  const [items, setItems] = React.useState<string[]>([]);
+  const [selectedItems, setSelectedItems] = React.useState<string[]>([]);
+
+  React.useEffect(() => {
+    setItems([]);
+    setSelectedItems([]);
+  }, [currentPage]);
+
+  const CheckboxBulkSelectAll: React.FC = () => (
+    <input
+      type="checkbox"
+      checked={items.length === selectedItems.length}
+      onChange={(e) => setSelectedItems(e.target.checked ? items : [])}
+    />
+  );
+
+  const CheckboxBulkSelectOne: React.FC<{ id: string }> = ({ id }) => {
+    React.useEffect(() => {
+      if (items.includes(id)) return; // already registered
+
+      setItems((items) => [...items, id]);
+    }, [id]);
+
+    const toggleItem = (e: React.ChangeEvent<HTMLInputElement>, id: string) => {
+      const checked = e.target.checked;
+
+      checked && setSelectedItems((oldSelectedItems) => [...oldSelectedItems, id]);
+      !checked && setSelectedItems((oldSelectedItems) => oldSelectedItems.filter((item) => item !== id));
+    };
+
+    return <input type="checkbox" onChange={(e) => toggleItem(e, id)} checked={selectedItems.includes(id)} />;
+  };
+
+  return { selectedItems, CheckboxBulkSelectAll, CheckboxBulkSelectOne };
+};
