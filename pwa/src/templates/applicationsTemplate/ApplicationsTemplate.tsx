f4520595287c6102f666a071098a76567aeb21e2
Lennart Bank <contact@lencodes.com>
2023-03-06 14:19:46 +0100

    IMPLEMENT useBulkSelect in Applications

--- pwa/src/templates/applicationsTemplate/ApplicationsTemplate.tsx
+++ pwa/src/templates/applicationsTemplate/ApplicationsTemplate.tsx
@@ -6,32 +6,28 @@ import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@
 import { navigate } from "gatsby";
 import { Container } from "@conduction/components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight, faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { translateDate } from "../../services/dateFormat";
 import { useQueryClient } from "react-query";
 import { useApplication } from "../../hooks/application";
 import Skeleton from "react-loading-skeleton";
 import { Button } from "../../components/button/Button";
 import { OverviewPageHeaderTemplate } from "../templateParts/overviewPageHeader/OverviewPageHeaderTemplate";
+import { useBulkSelect } from "../../hooks/useBulkSelect";
+import { BulkActionButton } from "../../components/bulkActionButton/BulkActionButton";
 
 export const ApplicationsTemplate: React.FC = () => {
   const { t, i18n } = useTranslation();
 
   const queryClient = useQueryClient();
   const _useApplication = useApplication(queryClient);
-  const getApplication = _useApplication.getAll();
+  const getApplications = _useApplication.getAll();
   const deleteApplication = _useApplication.remove();
 
-  const handleDeleteObject = (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent> | React.TouchEvent<HTMLButtonElement>,
-    objectId: string,
-  ) => {
-    e.stopPropagation();
-    const confirmDeletion = confirm("Are you sure you want to delete this object?");
+  const { CheckboxBulkSelectAll, CheckboxBulkSelectOne, selectedItems } = useBulkSelect(getApplications.data);
 
-    if (confirmDeletion) {
-      deleteApplication.mutate({ id: objectId });
-    }
+  const handleBulkDelete = (): void => {
+    selectedItems.forEach((item) => deleteApplication.mutate({ id: item }));
   };
 
   return (
@@ -49,53 +45,61 @@ export const ApplicationsTemplate: React.FC = () => {
         }
       />
 
-      {getApplication.isSuccess && (
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableHeader>{t("Name")}</TableHeader>
-              <TableHeader>{t("Description")}</TableHeader>
-              <TableHeader>{t("DateCreated")}</TableHeader>
-              <TableHeader>{t("DateModified")}</TableHeader>
-              <TableHeader></TableHeader>
-              <TableHeader></TableHeader>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {getApplication.data.map((application) => (
-              <TableRow onClick={() => navigate(`/settings/applications/${application.id}`)} key={application.id}>
-                <TableCell>{application.name}</TableCell>
-                <TableCell>{application.description ?? "-"}</TableCell>
-                <TableCell>{translateDate(i18n.language, application.dateCreated)}</TableCell>
-                <TableCell>{translateDate(i18n.language, application.dateModified)}</TableCell>
-                <TableCell>
-                  <Button
-                    variant="danger"
-                    icon={faTrash}
-                    label={t("Delete")}
-                    onClick={(e) => handleDeleteObject(e, application.id)}
-                  />
-                </TableCell>
-                <TableCell onClick={() => navigate(`/settings/applications/${application.id}`)}>
-                  <Link icon={<FontAwesomeIcon icon={faArrowRight} />} iconAlign="start">
-                    {t("Details")}
-                  </Link>
-                </TableCell>
-              </TableRow>
-            ))}
-            {!getApplication.data.length && (
+      {getApplications.isSuccess && (
+        <div>
+          <BulkActionButton
+            actions={[{ type: "delete", onSubmit: handleBulkDelete }]}
+            selectedItemsCount={selectedItems.length}
+          />
+
+          <Table>
+            <TableHead>
               <TableRow>
-                <TableCell>{t("No applications found")}</TableCell>
-                <TableCell />
-                <TableCell />
-                <TableCell />
-                <TableCell />
+                <TableHeader>
+                  <CheckboxBulkSelectAll />
+                </TableHeader>
+                <TableHeader>{t("Name")}</TableHeader>
+                <TableHeader>{t("Description")}</TableHeader>
+                <TableHeader>{t("DateCreated")}</TableHeader>
+                <TableHeader>{t("DateModified")}</TableHeader>
+                <TableHeader />
               </TableRow>
-            )}
-          </TableBody>
-        </Table>
+            </TableHead>
+            <TableBody>
+              {getApplications.data.map((application) => (
+                <TableRow key={application.id}>
+                  <TableCell>{<CheckboxBulkSelectOne id={application.id} />}</TableCell>
+
+                  <TableCell>{application.name}</TableCell>
+
+                  <TableCell>{application.description ?? "-"}</TableCell>
+
+                  <TableCell>{translateDate(i18n.language, application.dateCreated)}</TableCell>
+
+                  <TableCell>{translateDate(i18n.language, application.dateModified)}</TableCell>
+
+                  <TableCell onClick={() => navigate(`/settings/applications/${application.id}`)}>
+                    <Link icon={<FontAwesomeIcon icon={faArrowRight} />} iconAlign="start">
+                      {t("Details")}
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              ))}
+              {!getApplications.data.length && (
+                <TableRow>
+                  <TableCell>{t("No applications found")}</TableCell>
+                  <TableCell />
+                  <TableCell />
+                  <TableCell />
+                  <TableCell />
+                  <TableCell />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
       )}
-      {getApplication.isLoading && <Skeleton height="200px" />}
+      {getApplications.isLoading && <Skeleton height="200px" />}
     </Container>
   );
 };
