0bb7933b7806ef69364d5aab5751cc609f81516a
Remko <remko@conduction.nl>
2023-04-25 09:59:21 +0200

    refactored totalResultsSpan from string to number

--- pwa/src/components/totalResultsSpan/TotalResultsSpan.tsx
+++ pwa/src/components/totalResultsSpan/TotalResultsSpan.tsx
@@ -2,15 +2,15 @@ import * as React from "react";
 import * as styles from "./TotalResultsSpan.module.css";
 
 interface TotalResultsSpanProps {
-  total: string;
-  offset: string;
-  count: string;
+  total: number;
+  offset: number;
+  count: number;
 }
 
 export const TotalResultsSpan: React.FC<TotalResultsSpanProps> = ({ total, offset, count }) => {
   return (
     <span className={styles.content}>
-      {offset} tot {(offset + count)} van de {total} resultaten.
+      {offset} tot {offset + count} van de {total} resultaten.
     </span>
   );
 };
