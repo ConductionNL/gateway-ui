c5177ccb9e8c1b91a9b7ba12cf1b82d36daac324
Lennart Bank <contact@lencodes.com>
2023-05-23 09:14:22 +0200

    CLEANUP

--- pwa/static/env.js
+++ pwa/static/env.js
@@ -1,17 +1,8 @@
-window.sessionStorage.setItem("GATSBY_ME_URL", "https://api.larping.online/me");
-window.sessionStorage.setItem(
-  "GATSBY_API_URL",
-  "https://api.larping.online/api"
-);
-window.sessionStorage.setItem(
-  "GATSBY_ADMIN_URL",
-  "https://api.larping.online/admin"
-);
-window.sessionStorage.setItem("GATSBY_BASE_URL", "https://api.larping.online");
-window.sessionStorage.setItem(
-  "GATSBY_FRONTEND_URL",
-  "https://api.larping.online:8000"
-);
+window.sessionStorage.setItem("GATSBY_ME_URL", "http://localhost/me");
+window.sessionStorage.setItem("GATSBY_API_URL", "http://localhost/api");
+window.sessionStorage.setItem("GATSBY_ADMIN_URL", "http://localhost/admin");
+window.sessionStorage.setItem("GATSBY_BASE_URL", "http://localhost");
+window.sessionStorage.setItem("GATSBY_FRONTEND_URL", "http://localhost:8000");
 window.sessionStorage.setItem(
   "GATSBY_ORGANIZATION",
   "http://webresourcecatalogus.conduction.svc.cluster.local/organizations/b2d3176e-f1c6-4365-ab86-dd253c65fc43"
