e3615482450441fa0d736376017fc810781af327
Lencodes <contact@lencodes.com>
2023-05-25 15:16:52 +0200

    INIT UploadResourceActionsTemplate and some more layout

---
+++ pwa/src/templates/templateParts/uploadResourceActions/UploadResourceActionsTemplate.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as styles from "./UploadResourceActionsTemplate.module.css";
+
+export const UploadResourceActionsTemplate: React.FC = () => {
+  return <div className={styles.container}>Upload resources...</div>;
+};
--- pwa/src/templates/uploadTemplate/UploadTemplate.tsx
+++ pwa/src/templates/uploadTemplate/UploadTemplate.tsx
@@ -1,12 +1,45 @@
 import * as React from "react";
 import * as styles from "./UploadTemplate.module.css";
 import { Container } from "@conduction/components";
-import { Heading1 } from "@gemeente-denhaag/components-react";
+import { Alert, Heading1, Tab, TabContext, TabPanel, Tabs } from "@gemeente-denhaag/components-react";
+import { t } from "i18next";
+import { UploadResourceActionsTemplate } from "../templateParts/uploadResourceActions/UploadResourceActionsTemplate";
 
 export const UploadTemplate: React.FC = () => {
+  const [currentTab, setCurrentTab] = React.useState<number>(0);
+
   return (
     <Container layoutClassName={styles.container}>
       <Heading1 className={styles.title}>Upload and import</Heading1>
+
+      <Alert
+        title="Attention"
+        text="This page is availabel for development purposes only; it is not yet functional."
+        variant="warning"
+      />
+
+      <div className={styles.tabsContainer}>
+        <TabContext value={currentTab.toString()}>
+          <Tabs
+            value={currentTab}
+            onChange={(_, newValue: number) => {
+              setCurrentTab(newValue);
+            }}
+            variant="scrollable"
+          >
+            <Tab className={styles.tab} label={t("Upload")} value={0} />
+            <Tab className={styles.tab} label={t("Import")} value={1} />
+          </Tabs>
+
+          <TabPanel className={styles.tabPanel} value="0">
+            <UploadResourceActionsTemplate />
+          </TabPanel>
+
+          <TabPanel className={styles.tabPanel} value="1">
+            Import
+          </TabPanel>
+        </TabContext>
+      </div>
     </Container>
   );
 };
