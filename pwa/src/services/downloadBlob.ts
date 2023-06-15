e0469f5dd967c5dbe0c319a8c7b0f9f49c8d37a5
Remko <remko@conduction.nl>
2023-06-15 15:47:13 +0200

    cleanup

--- pwa/src/services/downloadBlob.ts
+++ pwa/src/services/downloadBlob.ts
@@ -1,10 +1,9 @@
 export const downloadAsPDF = (data: any, name: string): void => {
-    const url = window.URL.createObjectURL(new Blob([data]));
-    const link = document.createElement("a");
-    link.href = url;
-  
-    link.setAttribute("download", `${name}.pdf`);
-    document.body.appendChild(link);
-    link.click();
-  };
-  
+  const url = window.URL.createObjectURL(new Blob([data]));
+  const link = document.createElement("a");
+  link.href = url;
+
+  link.setAttribute("download", `${name}.pdf`);
+  document.body.appendChild(link);
+  link.click();
+};
